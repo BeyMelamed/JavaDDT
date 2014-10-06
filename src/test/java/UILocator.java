ed21a7f863dd6509cf5bf6cbac2d4723028713cb
Bey Melamed <beymelamed@gmail.com>
2014-10-05 23:31:09 -0400

    Remove methods now used in UILocator class

---
+++ src/test/java/UILocator.java
@@ -0,0 +1,12 @@
+/**
+ * Created by BeyMelamed on 10/5/2014.
+ * Selenium Based Automation Project
+ * Description
+ * <p/>
+ * When      |Who            |What
+ * ==========|===============|========================================================
+ * 10/5/2014  |Bey            |Initial Version
+ * ==========|===============|========================================================
+ */
+public class UILocator {
+}
